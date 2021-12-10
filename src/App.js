c8ffd09d01bf9a01e872c105f8205163f45d54cf
Sean Flynn <sean.flynn1773@gmail.com>
2021-12-10 15:29:36 -0500

    first push of app

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import './components/Overview';
+import Overview from './components/Overview';
+
+function App() {
+  return (
+    <div className="App">
+      <h1> Task App </h1>
+      <Overview />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Overview.js
@@ -0,0 +1,19 @@
+
+
+function Overview(){
+    return (
+        <div>
+            <form>
+                <input type="text" />
+                <button type="submit">Submit</button>
+            </form>
+            <div>
+                <button>View Tasks</button>
+            </div>
+        </div>
+    );
+}
+
+
+
+export default Overview;
